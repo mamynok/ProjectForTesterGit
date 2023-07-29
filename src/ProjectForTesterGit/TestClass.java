69b3afe7a84947fb48cb6b3377ce46d3f5dc384d
Karnpuntita Theematananun <karnpuntita.t@AAN.com>
2023-07-29 13:18:35 +0700

    I dont see the thrid line in second development

--- src/ProjectForTesterGit/TestClass.java
+++ src/ProjectForTesterGit/TestClass.java
@@ -4,7 +4,11 @@ public class TestClass {
 
 	public static void main(String[] args) {
 		System.out.println("this is a project of git");
-
+		System.out.println("I will put it on staging for git");
+		
+		
+		
+	
 	}
 
 }
