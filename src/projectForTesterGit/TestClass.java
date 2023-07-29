9ad34a7be80b00ec5b97ba071b06fe9ff5e0bab9
karnpuntita.t <karnpuntita.t@THGTNH064.AAN.com>
2023-07-29 12:04:24 +0700

    Second change

--- src/projectForTesterGit/TestClass.java
+++ src/projectForTesterGit/TestClass.java
@@ -5,6 +5,7 @@ public class TestClass {
 	public static void main(String[] args) {
 		System.out.println("this is a project of git");
 		System.out.println("I will put it on staging for git");
+		System.out.println("this is another change");
 
 	}
 
